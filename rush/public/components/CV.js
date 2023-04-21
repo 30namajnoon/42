2386f967311eaf36aeb56dd86713171507ec42de
330namajnoon <sina.majnoonhjk@gmail.com>
2023-04-21 15:23:17 +0200

    1

--- rush/public/components/CV.js
+++
@@ -1,8 +0,0 @@
-import createElement from "../js/createElement.js";
-import Navegacion from "./Navegacion.js";
-export default function CV() {
-   
-}
-CV.prototype.set = function(data) {
-    this.navegacion = new Navegacion();
-}
--- rush/public/js/data.js
+++
@@ -1,9 +0,0 @@
-const data = [
-    {
-        link:{name:"cv1",id:0}
-    },
-    {
-        link:{name:"cv2",id:1}
-    }
-]
-export default data;
